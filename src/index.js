b7e60d7006d418f8c0a700b36e4b46122c14e54f
ynkrpnk <ankarpenko35@gmail.com>
2026-05-17 11:30:35 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-lflfllf
+"use strict";
+
+const arrGroups = [
+  "FE2021-5",
+  "Fe2020-2",
+  "FD2021",
+  "fD2022-1",
+  "FE2020-3",
+  "FD2020/2",
+  "fe2021-3",
+  "FD2020$5",
+  "FE2008-52",
+  "FU2008-3",
+  "Fm2008-23",
+  "fm2008",
+];
+
+function checkGroups(groups) {
+  const regex = /^(FD|FM|FE|fd|fm|fe)20\d{2}(-\d+)?$/;
+
+  groups.forEach((group) => {
+    const message = regex.test(group)
+      ? `Success: ${group}`
+      : `Error:   ${group}`;
+
+    console.log(message);
+  });
+}
+
+checkGroups(arrGroups);
